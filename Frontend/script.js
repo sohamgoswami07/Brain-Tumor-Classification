9631a3f33fe4bc32e777bacef132d214e8a983c9
Soham Goswami <sohamgoswami07@gmail.com>
2024-09-03 20:59:15 +0530

    Processed photo problem remains only..

--- Frontend/script.js
+++ Frontend/script.js
@@ -1,17 +1,9 @@
-// function show_image(src, width, height, alt) {
-//     let img = document.createElement("img");
-//     img.src = "data:image/jpeg;base64,"+src;
-//     img.width = width;
-//     img.height = height;
-//     img.alt = alt;
-//     document.body.appendChild(img);
-// }
-
 function scanBtn() {
+    // Image processing function execution
     function show_image(src, width, height, alt) {
-        let canvas = document.createElement('canvas');
-        let ctx = canvas.getContext('2d');
-        let img = document.createElement('img');
+        let canvas = document.createElement("canvas");
+        let ctx = canvas.getContext("2d");
+        let img = document.createElement("img");
 
         img.src = "data:image/jpeg;base64," + src;
         img.width = width;
@@ -19,23 +11,23 @@ function scanBtn() {
         img.alt = alt;
 
         img.onload = function () {
-            // Set the canvas's width and height to match the image's dimensions
+            // Set the canvas"s width and height to match the image"s dimensions
             canvas.width = img.width;
             canvas.height = img.height;
 
-            ctx.filter = 'brightness(100%) contrast(100%) sepia(100%)' ;
+            ctx.filter = "brightness(100%) contrast(100%) sepia(100%)" ;
             ctx.imageSmoothingQuality = "high";
             ctx.drawImage(img, 0, 0, img.width, img.height); // Draw the image on the canvas
-            
 
-            // Convert canvas to data URL
-            let dataURL = canvas.toDataURL('image/jpeg', 0.9);
+            // Convert canvas to data URL to get jpeg format
+            let dataURL = canvas.toDataURL("image/jpeg", 0.9);
             photoCopy.src = dataURL;
         }
     }
 
-    let files = document.getElementById('myFiles').files;
-    let photoCopy = document.getElementById('photo-copy');
+    let files = document.getElementById("myFiles").files;
+    let photoCopy = document.getElementById("photo");
+    let resultsContainer = document.getElementById("results");
 
     for (let i = 0; i < files.length; i++) {
         (function (i) {
@@ -43,8 +35,8 @@ function scanBtn() {
             let formData = new FormData();
             formData.append("file", file);
 
-            fetch('http://localhost:8000/predict', {
-                method: 'POST',
+            fetch("http://localhost:8000/predict", {
+                method: "POST",
                 body: formData,
             })
                 .then(response => response.json())
@@ -56,23 +48,36 @@ function scanBtn() {
                         data.class= "Tumor found (Unidentified)";
                     }
 
-                    let fileName = document.getElementById('file-name');
-                    let clasification = document.getElementById('clasification');
-                    let confidence = document.getElementById('confidence');
-
-                    fileName.textContent = file.name;
-                    clasification.textContent = data.class;
-                    confidence.textContent = (data.confidence * 100).toFixed(2) + "%";
+                    let fileName = file.name;
+                    let clasification = data.class;
+                    let confidence = (data.confidence * 100).toFixed(2) + "%";
 
                     // Create a new progress bar for each file
-                    let progressBar = document.getElementById('progress-bar');
+                    let progressBar = (data.confidence * 100).toFixed(2);
                     progressBar.max = 100; // Set the maximum value
-                    progressBar.value = (data.confidence * 100).toFixed(2);
 
+                    let results = document.createElement("div");
+                    results.classList.add("file-result"); // You can style this class as needed
+
+                    results.innerHTML = `
+                        <div id="photo-copy">
+                            <img id="photo" src="${photoCopy}">
+                        </div>
+                        <div id="result-details">
+                            <strong id="result-values">File Name: <p>${fileName}</p></strong>
+                            <strong id="result-values">Classification: <p>${clasification}</p></strong>
+                            <strong id="result-values">Confidence: <p>${confidence}</p></strong>
+                            <progress id="progress-bar" value=${progressBar} max="100"></progress>
+                        </div>
+                    `;
+
+                    resultsContainer.appendChild(results);
+
+                    // Image processing function call
                     show_image(data.image, 250, 250, data.class);
                 })
                 .catch((error) => {
-                    console.error('Error:', error);
+                    console.error("Error:", error);
                 });
         })(i);
     }
